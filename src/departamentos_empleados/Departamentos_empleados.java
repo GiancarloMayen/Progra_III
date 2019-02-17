b6308478d27a50cdd0d21699b05e8aced24dc7bd
GiancarloMayen <Giancarlomayen210@gmail.com>
2019-02-17 13:01:40 -0800

    nomina v1.20 cambio en los tipos de datos,agregamos mas elementos a la matriz, arreglar 2 decimales

--- src/departamentos_empleados/Departamentos_empleados.java
+++ src/departamentos_empleados/Departamentos_empleados.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 import javax.swing.JOptionPane;
 public class Departamentos_empleados {
 
-    /* Este programa esta diseñado para calcular los sueldos de los empleados de una empresa en forma aleatoria,
+    /* Este programa esta diseÃƒÂ±ado para calcular los sueldos de los empleados de una empresa en forma aleatoria,
         para esto utilizamos vectores,matrices,que nos ayudan a poder realizar el trabajo de una forma mas rapida
         con la ayuda de varios ciclos for.
         
     */
     public static void main(String[] args) {
-        
+        final float IGSS=4.83f;
         Scanner entrada=new Scanner(System.in);//declaramos una variable de tipo scanner 
         
         //declaramos un vector de tipo String con diferentes nombres de los empleados ya ingresados en el vector
@@ -17,7 +17,18 @@ public class Departamentos_empleados {
         //creamos un vector de 5 posiciones que contiene los nombres de cada departamento de la empresa
         String [] strDepartamentos={"Ingenieria","Recursos H.","Contabilidad","Auditoria","Huachicolero"};
         //declaramos una matriz de tipo short con dimensiones de: 10 filas y 5 columnas
-        short matrix[][]=new short[10][5];
+        float matrix[][]=new float[10][8];
+        
+        
+        /*columnas de la matriz:
+        1:  van los sueldos de las personas
+        2:  van las bonificaciones de las personas 250
+        3:  van las comisiones de las personas
+        4:  van el igss de las personas
+        5:  van el isr de las personas
+        6:  va el descuento del bantrab
+        */
+        
         
         
         
@@ -25,36 +36,66 @@ public class Departamentos_empleados {
         //creamos un ciclo for que nos ayude a llenar la matriz con cada sueldo y descuento en sus respectivas posiciones       
         for(int z=0;z<10;z++){
             //aqui declaramos variables de tipo short que nos guardan de forma aleatoria el sueldo,descuento,etc en cada variable
-            short shrSueldos=(short)((Math.random()*6000)+2000);
-            short shrDescuentos=(short)((Math.random()*500)+200);
-            short shrBonificaciones=(short)((Math.random()*700)+200);
+            float ftSueldos=(float)((Math.random()*15000)+2000);
+            
+            float fBonificaciones=250;
+            
+            float shrComisiones=(float)((Math.random()*700)+200);
+            
+            float shrIGSS=(float)(ftSueldos*IGSS)/100;
+            
+            float isr=0;
+            if(ftSueldos>2600 && ftSueldos<5000){
+                isr=(float)(ftSueldos*3)/100;
+            }
+            else if(ftSueldos>5001 && ftSueldos<10000){
+                isr=(float)(ftSueldos*5)/100;
+            }
+            else if(ftSueldos>10001){
+                isr=(float)(ftSueldos*10)/100;
+            }
+            
+            float fBancoTrabajadores=(float)((Math.random()*1000)+2000);
+            
             byte btDepto=(byte) ((Math.random()*5)+1);
-            short shrTotal=(short) (shrSueldos+shrBonificaciones-shrDescuentos);
+            
+            float shrTotal=(float) ((ftSueldos+fBonificaciones+shrComisiones)-(shrIGSS+isr+fBancoTrabajadores));
+            
             //y estos a su vez se guardan el cada posicion de la matriz especificada
-            matrix[z][0]=shrSueldos;
-            matrix[z][1]=shrDescuentos;
-            matrix[z][2]=shrBonificaciones;
-            matrix[z][3]=shrTotal;
-            matrix[z][4]=btDepto;
+            matrix[z][0]=ftSueldos;
+            matrix[z][1]=fBonificaciones;
+            matrix[z][2]=shrComisiones;
+            matrix[z][3]=shrIGSS;
+            matrix[z][4]=isr;
+            matrix[z][5]=fBancoTrabajadores;
+            matrix[z][6]=shrTotal;
+            matrix[z][7]=btDepto;
             
         }//fin del ciclo for
+        
+        
+        
         
         //mostramos un OptionPane en pantalla que nos indica cuales son los departamentos que existen clasificandolos por numeros
         JOptionPane.showMessageDialog(null, "los departamentos que exiten son:\n1: Ingenieria\n2: RRHH\n3: Contabilidad\n4: Auditoria\n5: Huachicolero");
         
         
         //mostramos 2 mensajes,1 que  nos indican los titulos de las columnas
-        System.out.println("Trabajador\tSueldo -   Descuentos  +  Bonif.  =  Total      Depto.");
+        System.out.println("          \t\t              Bonificaciones           -             Descuentos                                              ");
+        System.out.println("Trabajador\tSueldo B.    + (Bonif.   +    comisiones) - (IGSS         +     ISR   + BT        )   =   total     Depto.");
         //y el otro que nos separa el titulo con unas lineas para diferenciar los datos
         System.out.println("------------------------------------------------------------------------");
+        
+        
+        
         //ciclo for para recorrer la matriz
         for(int a=0;a<10;a++){//primer ciclo que recorre las filas
             //System.out.println();println que nos deja
             System.out.print(strNombres[a]+"\t\t ");//primer mensaje que nos muestra el vector de nombres en pantalla
             
-            for(int b=0;b<5;b++){//inicico del segundo ciclo for que recorre las columnas
+            for(int b=0;b<8;b++){//inicico del segundo ciclo for que recorre las columnas
                 
-                System.out.print(matrix[a][b]+"         ");//mensaje en pantalla que nos muestra la matriz con sus respectivos valores
+                System.out.printf(matrix[a][b]+"           ");//mensaje en pantalla que nos muestra la matriz con sus respectivos valores
             }//fin del segundo ciclo for
             //salto de linea para separar cada fila
             System.out.println();
@@ -64,39 +105,40 @@ public class Departamentos_empleados {
         short shrSuma_sueldos1=0,shrSuma_sueldos2=0,shrSuma_sueldos3=0,shrSuma_sueldos4=0,shrSuma_sueldos5=0;
         
 //creamos un ciclo for que recorrera la columna 4 de la matriz en donde estan guardados el numero de departamento para poder sumar los sueldos
+
         for(int i=0;i<10;i++){//ciclo for que recorre la matriz
             byte btOpc;//creamos una variable de tipo bite
-            btOpc=(byte)matrix[i][4];//aqui igualamos la variable bite al valor que esta en la fila del 1 al 10 y la columna 4
+            btOpc=(byte)matrix[i][7];//aqui igualamos la variable bite al valor que esta en la fila del 1 al 10 y la columna 4
             //creamos un switch poniendo como parametro la variable byte
             switch(btOpc){//inicio del switch
                 
                 case 1:{//caso 1 si la variable es igual a 1 que significa que pertenece al departamento de Ingenieria
                     //ponemos que la variable=variable mas el valor que tiene el sueldo en la matriz
-                    shrSuma_sueldos1=(short)(shrSuma_sueldos1+matrix[i][3]);
+                    shrSuma_sueldos1=(short)(shrSuma_sueldos1+matrix[i][6]);
                     break;//break
                 }//fin del caso 1
                 
                 case 2:{//caso 2 si la variable es igual a 1 que significa que pertenece al departamento de Recursos Humanos
                     //ponemos que la variable=variable mas el valor que tiene el sueldo en la matriz
-                    shrSuma_sueldos2=(short)(shrSuma_sueldos1+matrix[i][3]);
+                    shrSuma_sueldos2=(short)(shrSuma_sueldos1+matrix[i][6]);
                     break;
                 }//fin del caso 2
                 
                 case 3:{//caso 3 si la variable es igual a 1 que significa que pertenece al departamento de Contabilidad
                     //ponemos que la variable=variable mas el valor que tiene el sueldo en la matriz
-                    shrSuma_sueldos3=(short)(shrSuma_sueldos1+matrix[i][3]);
+                    shrSuma_sueldos3=(short)(shrSuma_sueldos1+matrix[i][6]);
                     break;
                 }//fin del caso 3
                 
                 case 4:{//caso 4 si la variable es igual a 1 que significa que pertenece al departamento de Auditoria
                     //ponemos que la variable=variable mas el valor que tiene el sueldo en la matriz
-                    shrSuma_sueldos4=(short)(shrSuma_sueldos1+matrix[i][3]);
+                    shrSuma_sueldos4=(short)(shrSuma_sueldos1+matrix[i][6]);
                     break;
                 }//fin del caso 4
                 
                 case 5:{//caso 5 si la variable es igual a 1 que significa que pertenece a los huachicoleros
                     //ponemos que la variable=variable mas el valor que tiene el sueldo en la matriz
-                    shrSuma_sueldos5=(short)(shrSuma_sueldos1+matrix[i][3]);
+                    shrSuma_sueldos5=(short)(shrSuma_sueldos1+matrix[i][6]);
                     break;
                 }//fin del caso 5
                 
@@ -128,12 +170,12 @@ public class Departamentos_empleados {
         //creamos un ciclo for que nos muestre el vector con los sueldos sumados en el for anterior
         for(int j=0;j<5;j++){//inicio del ciclo for
             //mostramos en pantalla los 2 vectores
-            System.out.println("\t\t"+(j+1)+"\t"+strDepartamentos[j]+"\t "+shrVector[j]);
+            System.out.println("\t\t"+(j+1)+"\t"+strDepartamentos[j]+"\t"+shrVector[j]);
         }//fin del ciclo for
         
         
         
-        
+        //falta arreglar lo de los float, no me deja ponerle solo 2 decimales, solo coregir eso y ordenar los numeros y columnas
     }
     
 }
